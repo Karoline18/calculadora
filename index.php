14e39c87171dac16576ed22d0f9f4ed3c1970b6b
Cache Sistemas <37821990+cachesistemasweb@users.noreply.github.com>
2018-03-28 13:12:40 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,9 @@
+  <?php
+  
+  include("formulariocalculadora.html");
+  include("resultado.php");
+  include("estilo.txt");
+ 
+  ?>
+  <div id="resultado"><?php echo((isset($resultado))?"".$resultado."":""); ?></div>
 
- <?php
-
-
-  $numero1;
-  $numero2;
-
- ?>
- <!-- Formulário da Calculadora -->
-  <link rel="stylesheet" href="estilo.css">
-  <div id="formatocal">
-  <form method="post" action="index.php">
-  <input type="text" name="numero1" id="numero1" maxlength="50" />
-  <input type="num" name="numero2" id="numero2" maxlength="50" /><br>
-  <input type="radio" name="opera"   value="somar" /><label>Somar</label><br>
-  <input type="radio" name="opera"   value="multiplica" /><label>Multipicar</label><br>
-  <input type="radio" name="opera"   value="dividir" /><label>Dividir</label><br>
-  <input type="radio" name="opera"   value="subitrai" /><label>Subitrair</label><br>
-  <input type="submit" value="SOMAR" />
-</div>
-</form>
-<? 
-// incluindo a página que faz as comparações e operações 
-include("resultado.php"); ?>
-<? 
-//imprimindo o resuldado 
-echo((isset($resultado))?"<h1>".$resultado."</h1>":""); ?>
